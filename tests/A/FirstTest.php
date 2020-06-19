f820d533a151098c70e4f9a46c26c43db137dff2
Leonid Meleshin <lm@devolt.one>
2020-06-19 13:58:02 +0300

    tests: add first example test

---
+++ tests/A/FirstTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Devolt\Restful\Tests\A;
+
+use PHPUnit\Framework\TestCase;
+
+class FirstTest extends TestCase
+{
+    /**
+     * @testdox Consumes testing bootstrap time
+     * @test
+     */
+    public function testBootstrapTime()
+    {
+        $this->assertTrue(true);
+    }
+}
